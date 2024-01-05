0b4dbb2a8c65300dec8945978bf1dad0db21877f
Aosama <ahmed.osama@elsewedyintel.com>
2024-01-05 02:02:23 +0200

    add var load

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """add function
+
+        a: first number
+        b: second number
+
+    The return . a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """sub function
+
+        a: first number
+        b: second number
+
+    The return . a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """multi function
+
+        a: first number
+        b: second number
+
+    The return . a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """div function
+
+        a: first number
+        b: second number
+
+    The return . a / b
+    """
+    return int(a / b)
