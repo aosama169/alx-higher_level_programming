c9b68c0918ae79c5a07bef961d655d5149856813
Aosama <ahmed.osama@elsewedyintel.com>
2024-02-04 21:19:27 +0200

    add task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+""" class Square that define square"""
+
+
+class Square:
+    """ class Square that define square"""
+    def __init__(self, size=0):
+        """ init square
+
+        Args:
+            value (int): size of squar
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """int: private size
+
+        Returns:
+            Private size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets value into size, must be int
+
+        Args:
+            value (int): size of squar
+        """
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value  #: size of squar
+
+    def area(self):
+        """returns area
+
+        Returns:
+            area
+        """
+        return self.__size**2
+
+    def my_print(self):
+        """prints in stdout square with char hash"""
+
+        if self.__size != 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end='')
+                print()
+        else:
+            print()
