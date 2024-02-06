f85cd17e08ff40ccc08e67bff1678a9c864d59c1
Aosama <ahmed.osama@elsewedyintel.com>
2024-02-06 18:52:49 +0200

    task 4

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""return True if  object is exactly  instance of  specified class
+"""
+
+
+def is_same_class(obj, a_class):
+    """return True if object is exact instance"""
+    return True if type(obj) == a_class else False
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module check class and subclass
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    returns True if the object is an instance of class
+    """
+    return isinstance(obj, a_class)
