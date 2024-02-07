4cda179126cd2ebbbb3cc647460fdef751fad29b
Aosama <ahmed.osama@elsewedyintel.com>
2024-02-07 17:34:01 +0200

    fix indents

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""  
+"""
 	return list of available attrs and methods
-	
 """
 
 
 def lookup(obj):
-    """return list of available attrs"""
-    return dir(obj)
+	"""return list of available attrs"""
+	return dir(obj)
