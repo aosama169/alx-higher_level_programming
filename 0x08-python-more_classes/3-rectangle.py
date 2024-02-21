cf77ef7d63e254e76151f448e1dedd5ee86e07a0
Aosama <ahmed.osama@elsewedyintel.com>
2024-02-21 23:49:27 +0200

    task 4

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
+""" class Rectangle that define rectangle
+"""
+
 
 class Rectangle:
+    """ class rectangles"""
     def __init__(self, width=0, height=0):
+        """ Instantiation with optional width and height int"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """ width int
+        """
         return self.__width
 
     @property
     def height(self):
+        """ height int
+        """
         return self.__height
 
     @width.setter
     def width(self, value):
+        """ width setter int
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -23,6 +34,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """ height setter int
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -30,14 +43,18 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """ rectangle area int"""
         return self.__width * self.__height
 
     def perimeter(self):
+        """ rectangle perimiter int"""
         if self.__width is 0 or self.__height is 0:
             return 0
         return self.__width * 2 + self.__height * 2
 
     def __str__(self):
+        """ the rectangle with the character int 
+        """
         if self.__width is 0 or self.__height is 0:
             return ""
         return ("\n".join(["".join(["#" for i in range(self.__width)])
--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
+""" empty class Rectangle define rectangle
+"""
 
 
 class Rectangle:
+    """ class rectangles"""
     def __init__(self, width=0, height=0):
+        """ Instante with option width and height int"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """ width int
+        """
         return self.__width
 
     @property
     def height(self):
+        """ height int
+        """
         return self.__height
 
     @width.setter
     def width(self, value):
+        """ width setter int
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -24,6 +34,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """ height setter int
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -31,18 +43,24 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """ rectangle area int"""
         return self.__width * self.__height
 
     def perimeter(self):
+        """ rectangle perimiter"""
         if self.__width is 0 or self.__height is 0:
             return 0
         return self.__width * 2 + self.__height * 2
 
     def __str__(self):
+        """ rectangle with character #
+        """
         if self.__width is 0 or self.__height is 0:
             return ""
         return ("\n".join(["".join(["#" for i in range(self.__width)])
                 for j in range(self.__height)]))
 
     def __repr__(self):
+        """ string representation of rectangle
+        """
         return "Rectangle({}, {})".format(self.__width, self.__height)
