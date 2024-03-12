c7ee999e25b8eb0a7ed3e6019d512c74f2fef4b2
Aosama <ahmed.osama@elsewedyintel.com>
2024-03-13 00:22:33 +0200

    optional tasks

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- Display 3 cities with highest average
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
