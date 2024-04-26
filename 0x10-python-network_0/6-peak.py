70ac68470c01d5bda131b2007370b34b3746c420
Aosama <ahmed.osama@elsewedyintel.com>
2024-04-27 01:23:29 +0300

    fix pycode style

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """Find peak in list of unsorted int"""
 
-def find_peak(list_of_integers):
+
+def find_peak(list_of_ints):
     """Find peak in list_of_int"""
 
-    if list_of_integers is None or list_of_integers == []:
+    if list_of_ints is None or list_of_ints == []:
         return None
     lowInt = 0
-    hi = len(list_of_integers)
+    hi = len(list_of_ints)
     mediums = ((hi - lowInt) // 2) + lowInt
     mediums = int(mediums)
     if hi == 1:
-        return list_of_integers[0]
+        return list_of_ints[0]
     if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mediums] >= list_of_integers[mediums - 1] and\
-            list_of_integers[mediums] >= list_of_integers[mediums + 1]:
-        return list_of_integers[mediums]
-    if mediums > 0 and list_of_integers[mediums] < list_of_integers[mediums + 1]:
-        return find_peak(list_of_integers[mediums:])
-    if mediums > 0 and list_of_integers[mediums] < list_of_integers[mediums - 1]:
-        return find_peak(list_of_integers[:mediums])
+        return max(list_of_ints)
+    if list_of_ints[mediums] >= list_of_ints[mediums - 1] and\
+            list_of_ints[mediums] >= list_of_ints[mediums + 1]:
+        return list_of_ints[mediums]
+    if mediums > 0 and list_of_ints[mediums] < list_of_ints[mediums + 1]:
+        return find_peak(list_of_ints[mediums:])
+    if mediums > 0 and list_of_ints[mediums] < list_of_ints[mediums - 1]:
+        return find_peak(list_of_ints[:mediums])
